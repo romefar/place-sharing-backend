4b9fee4a6119683dcc4d5b2b56f6f6f1ad545116
romefar <vlad.romanovr@gmail.com>
2020-06-02 20:34:08 +0300

    Added places routes

---
+++ src/routes/places.js
@@ -0,0 +1,25 @@
+const express = require('express')
+const { check } = require('express-validator')
+const router = express.Router()
+const {
+  getPlaceById,
+  getPlacesByUserId,
+  createPlace,
+  updatePlace,
+  deletePlace
+} = require('../controllers/places')
+
+router.get('/:pid', getPlaceById)
+router.get('/user/:uid', getPlacesByUserId)
+router.post('/', [
+  check('title').not().isEmpty(),
+  check('description').isLength({ min: 5 }),
+  check('address').not().isEmpty()
+], createPlace)
+router.patch('/:pid', [
+  check('title').not().isEmpty(),
+  check('description').isLength({ min: 5 })
+], updatePlace)
+router.delete('/:pid', deletePlace)
+
+module.exports = router
