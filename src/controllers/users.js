bd807bb709d5556d025b7260f2ed5fd5397a164a
romefar <vlad.romanovr@gmail.com>
2020-06-02 20:31:49 +0300

    Added user controller

---
+++ src/controllers/users.js
@@ -0,0 +1,56 @@
+const HttpError = require('../models/http-error')
+const { validationResult } = require('express-validator')
+const User = require('../models/user')
+
+const getUsers = async (req, res, next) => {
+  try {
+    const users = await User.find({}, '-password')
+    res.send({ users })
+  } catch (error) {
+    return next(error)
+  }
+}
+
+const signUp = async (req, res, next) => {
+  try {
+    const errors = validationResult(req)
+    if (!errors.isEmpty()) {
+      throw new HttpError('Invalid inputs passed.', 422)
+    }
+    const { name, email, password } = req.body
+    const exUser = await User.findOne({ email })
+    if (exUser) {
+      throw new HttpError('User is already exists.', 422)
+    }
+    const user = new User({
+      name,
+      email,
+      password,
+      image: 'https://www.dovercourt.org/wp-content/uploads/2019/11/610-6104451_image-placeholder-png-user-profile-placeholder-image-png.jpg',
+      places: []
+    })
+    await user.save()
+    res.status(201).send({ user })
+  } catch (error) {
+    return next(error)
+  }
+}
+
+const login = async (req, res, next) => {
+  try {
+    const { email, password } = req.body
+    const user = await User.findOne({ email, password })
+    if (!user || user.password !== password) {
+      throw new HttpError('Couldn\'t identify a user.', 401)
+    }
+    res.send({ message: 'Logged in.' })
+  } catch (error) {
+    return next(error)
+  }
+}
+
+module.exports = {
+  signUp,
+  login,
+  getUsers
+}
