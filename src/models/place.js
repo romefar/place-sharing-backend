8145534c2122329f307bbdccd9764b08e96eda70
romefar <vlad.romanovr@gmail.com>
2020-06-02 20:33:30 +0300

    Added place model

---
+++ src/models/place.js
@@ -0,0 +1,38 @@
+const { Schema, model, Types } = require('mongoose')
+
+const placeSchema = new Schema({
+  title: {
+    type: String,
+    required: true
+  },
+  description: {
+    type: String,
+    required: true,
+    minlength: 5
+  },
+  image: {
+    type: String,
+    required: true
+  },
+  address: {
+    type: String,
+    required: true
+  },
+  location: {
+    lat: {
+      type: Number,
+      required: true
+    },
+    lng: {
+      type: Number,
+      required: true
+    }
+  },
+  creatorId: {
+    type: Types.ObjectId,
+    required: true,
+    ref: 'User'
+  }
+})
+
+module.exports = model('Place', placeSchema)
