f1b4ab86db17d74fc146e863b5bac03d290e1247
romefar <vlad.romanovr@gmail.com>
2020-06-04 20:18:42 +0300

    Added auth, file upload

--- src/routes/places.js
+++ src/routes/places.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const { check } = require('express-validator')
 const router = express.Router()
+const chekAuth = require('../middleware/check-auth')
+const fileUpload = require('../middleware/file-upload')
 const {
   getPlaceById,
   getPlacesByUserId,
@@ -9,17 +11,39 @@ const {
   deletePlace
 } = require('../controllers/places')
 
-router.get('/:pid', getPlaceById)
-router.get('/user/:uid', getPlacesByUserId)
-router.post('/', [
-  check('title').not().isEmpty(),
-  check('description').isLength({ min: 5 }),
-  check('address').not().isEmpty()
-], createPlace)
-router.patch('/:pid', [
-  check('title').not().isEmpty(),
-  check('description').isLength({ min: 5 })
-], updatePlace)
-router.delete('/:pid', deletePlace)
+router.get(
+  '/:pid',
+  getPlaceById
+)
+
+router.get(
+  '/user/:uid',
+  getPlacesByUserId
+)
+
+router.post(
+  '/',
+  chekAuth,
+  fileUpload.single('image'),
+  [
+    check('title').not().isEmpty(),
+    check('description').isLength({ min: 5 }),
+    check('address').not().isEmpty()
+  ],
+  createPlace)
+
+router.patch(
+  '/:pid',
+  chekAuth,
+  [
+    check('title').not().isEmpty(),
+    check('description').isLength({ min: 5 })
+  ],
+  updatePlace)
+
+router.delete(
+  '/:pid',
+  chekAuth,
+  deletePlace)
 
 module.exports = router
--- src/routes/users.js
+++ src/routes/users.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const { check } = require('express-validator')
+const fileUpload = require('../middleware/file-upload')
 const router = express.Router()
 const {
   signUp,
@@ -8,11 +9,13 @@ const {
 } = require('../controllers/users')
 
 router.get('/', getUsers)
-router.post('/signup', [
-  check('name').not().isEmpty(),
-  check('email').normalizeEmail().isEmail(),
-  check('password').isLength({ min: 7 })
-], signUp)
+router.post('/signup',
+  fileUpload.single('image'),
+  [
+    check('name').not().isEmpty(),
+    check('email').normalizeEmail().isEmail(),
+    check('password').isLength({ min: 7 })
+  ], signUp)
 router.post('/login', login)
 
 module.exports = router
