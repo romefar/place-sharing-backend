348d780d3629cb6ca1796926d23fe7925bdb85d2
romefar <vlad.romanovr@gmail.com>
2020-06-04 20:16:30 +0300

    Added image upload

--- src/controllers/places.js
+++ src/controllers/places.js
@@ -1,6 +1,7 @@
 const HttpError = require('../models/http-error')
 const Place = require('../models/place')
 const User = require('../models/user')
+const fs = require('fs')
 const { startSession } = require('mongoose')
 const { validationResult } = require('express-validator')
 
@@ -41,20 +42,20 @@ const createPlace = async (req, res, next) => {
     if (!errors.isEmpty()) {
       throw new HttpError('Invalid inputs passed.', 422)
     }
-    const { title, description, address, creatorId } = req.body
+    const { title, description, address } = req.body
     createdPlace = new Place({
       title,
       description,
       address,
-      image: 'https://upload.wikimedia.org/wikipedia/commons/6/6c/Looking_Up_at_Empire_State_Building.JPG',
+      image: req.file.path,
       location: { // Replace with a geocoder later (OpenCage or etc.)
         lat: 40.7484445,
         lng: -73.9878531
       },
-      creatorId
+      creatorId: req.userData.userId
     })
 
-    const user = await User.findById(creatorId)
+    const user = await User.findById(req.userData.userId)
     if (!user) {
       throw new HttpError('Could\'t find a user.', 404)
     }
@@ -78,6 +79,7 @@ const createPlace = async (req, res, next) => {
 
 const updatePlace = async (req, res, next) => {
   try {
+    const { userId } = req.userData
     const errors = validationResult(req)
     if (!errors.isEmpty()) {
       throw new HttpError('Invalid inputs passed.', 422)
@@ -85,7 +87,7 @@ const updatePlace = async (req, res, next) => {
     const { title, description } = req.body
     const { pid } = req.params
 
-    const updatedPlace = await Place.findByIdAndUpdate(pid, { title, description })
+    const updatedPlace = await Place.findOneAndUpdate({ _id: pid, creatorId: userId }, { title, description })
 
     if (!updatedPlace) {
       throw new HttpError('Couldn\'t find a place for that id.', 404)
@@ -100,13 +102,19 @@ const updatePlace = async (req, res, next) => {
 const deletePlace = async (req, res, next) => {
   try {
     const { pid } = req.params
+    const { userId } = req.userData
     const place = await Place
-      .findById(pid)
+      .findOne({
+        _id: pid,
+        creatorId: userId
+      })
       .populate('creatorId')
 
     if (!place) {
       throw new HttpError('Couldn\' find a place for that id.', 404)
     }
+
+    const imagePath = place.image
 
     // Transactions example
     // You MUST create a 'places' collection manually
@@ -118,6 +126,8 @@ const deletePlace = async (req, res, next) => {
     place.creatorId.places.pull(place)
     await place.creatorId.save({ session })
     await session.commitTransaction()
+
+    fs.unlink(imagePath, (err) => console.log(err))
 
     res.send({ place })
   } catch (error) {
